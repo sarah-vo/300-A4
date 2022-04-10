3aff0cdb4d7852ee4a10027b1cffd1c5f7d56c32
Sarah Vo <tsvo@sfu.ca>
2022-04-10 16:13:56 -0700

    Complete traversal code, incorrect(?) largest delay code

--- main.c
+++ main.c
@@ -16,6 +16,10 @@ List* list_scan;
 int* rand_int;
 int* sorted;
 
+//search comparator
+COMPARATOR_FN pComparatorFn = &list_comparator;
+
+
 
 
 /**
@@ -42,7 +46,6 @@ void initialize_lists(){
 
     // printf("total: %d\n\n", total);
 
-    COMPARATOR_FN pComparatorFn = &list_comparator;
     int i = 0;
     // printf("\nfcfs: \n");
     while(List_count(list_fcfs) != total){
@@ -89,6 +92,69 @@ void list_item_free(void* pItem){
     pItem = NULL;
 }
 
+int totalTrack(List* list){
+    int returnValue = 0;
+    List_first(list);
+    while(List_next(list) != NULL){
+        List_prev(list);
+        int track = *(int*)List_curr(list) - *(int*) List_next(list);
+        returnValue += abs(track);
+    }
+    return returnValue;
+}
+
+int longestDelayLoop(List* list, int next_fcfs){
+    int delay = 0;
+    int origValue = *(int*)List_curr(list);
+    while(true){
+        if(*(int*)List_curr(list) == next_fcfs){
+            break;
+        }
+        delay++;
+        if(List_curr(list) == NULL || List_next(list) == NULL){
+            List_first(list);
+        }
+        else{
+            List_prev(list);
+        }
+        if(*(int*)List_next(list) == next_fcfs){
+            break;
+        }
+
+    }
+    return delay;
+}
+
+int longestDelay(List* list){
+    int largestDelay = 0;
+    List_first(list);
+    List_first(list_fcfs);
+    while(List_next(list_fcfs) != NULL){
+        //reset fcfs position
+        List_prev(list_fcfs);
+
+        int curr_fcfs = *(int*)List_curr(list_fcfs);
+        int next_fcfs = *(int*)List_next(list_fcfs);
+        if(list == list_fcfs){
+            List_prev(list);
+        }
+        int currValue = *(int*)List_curr(list);
+        bool correctPos = (List_search(list, pComparatorFn, &curr_fcfs) != NULL);
+        if(correctPos){
+            int delay = longestDelayLoop(list, next_fcfs);
+            if(delay >= largestDelay){
+                largestDelay = delay;
+            }
+        }
+        else{
+            perror("Cannot find fcfs value in the algorithm!!\n");
+        }
+    }
+    return largestDelay;
+}
+
+
+
 int main(){
     initialize_lists();
     Node* head = List_first(list_fcfs);    // first item in the list = head
@@ -99,10 +165,19 @@ int main(){
     // sort list_fcfs and pass it to disk scheduling function
     list_scan = disk_scan(list_sorted, head);
     list_cscan = disk_cscan(list_sorted, head);
+
+    printf("Total number of tracks traversed for each algorithm:\n");
+    printf("  - FCFS: %d tracks traversed.\n", totalTrack(list_fcfs));
+    printf("  - SCAN: %d tracks traversed.\n", totalTrack(list_scan));
+    printf("  - CSCAN: %d tracks traversed.\n", totalTrack(list_cscan));
+
+    printf("Longest track delay for each algorithm compared to FCFS:\n");
+    printf("  - SCAN: %d\n", longestDelay(list_scan));
+    printf("  - CSCAN: %d\n", longestDelay(list_cscan));
+
     
-    
-    // List_free(list_fcfs, &list_item_free);
-    // List_free(list_sorted, &list_item_free);
+     List_free(list_fcfs, &list_item_free);
+     List_free(list_sorted, &list_item_free);
 
     return 0;
 }
